234c894691073fc331368804d70ef39bc3d6727d
Simon Fessehaye <fessehay@ualberta.ca>
2021-11-24 03:05:26 -0800

    fix

--- workbox-config.js
+++ workbox-config.js
@@ -8,6 +8,26 @@ const buildSW = () => {
     globPatterns: ["**/*.{css}"],
     ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
     swDest: "dist/sw.js",
+
+    runtimeCaching: [
+      {
+        // Match any request that ends with .png, .jpg, .jpeg or .svg.
+        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+        // Apply a cache-first strategy.
+        handler: "CacheFirst",
+
+        options: {
+          // Use a custom cache name.
+          cacheName: "images",
+
+          // Only cache 10 images.
+          expiration: {
+            maxEntries: 10,
+          },
+        },
+      },
+    ],
   });
 };
 
