4f2d2c9978b3a86868369c189b6a147bc5e2c442
Simon Fessehaye <fessehay@ualberta.ca>
2021-11-24 17:37:32 -0800

    change workbox config

--- workbox-config.js
+++ workbox-config.js
@@ -5,29 +5,11 @@ const buildSW = () => {
   // This will return a Promise
   return workboxBuild.generateSW({
     globDirectory: "dist",
-    globPatterns: ["**/*.{css}"],
+    globPatterns: ["**/*.{css,html}"],
     ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
     swDest: "dist/sw.js",
 
-    runtimeCaching: [
-      {
-        // Match any request that ends with .png, .jpg, .jpeg or .svg.
-        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-
-        // Apply a cache-first strategy.
-        handler: "CacheFirst",
-
-        options: {
-          // Use a custom cache name.
-          cacheName: "images",
-
-          // Only cache 10 images.
-          expiration: {
-            maxEntries: 10,
-          },
-        },
-      },
-    ],
+    runtimeCaching: [],
   });
 };
 
