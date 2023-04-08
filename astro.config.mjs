93e47d3e52ad2b6361399479f5ac8a42875f775c
fessehaye <fessehay@ualberta.ca>
2023-04-07 19:55:30 -0700

    added AstroPWA plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import tailwind from "@astrojs/tailwind";
+import AstroPWA from "@vite-pwa/astro";
 
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
   // Enable Tailwind by telling Astro where your Tailwind config file lives.
-  integrations: [tailwind()],
+  integrations: [tailwind(), AstroPWA()],
 });
